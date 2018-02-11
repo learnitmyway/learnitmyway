fabb27ee2ac33dc97c2c780617e90f057aac7383
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-02-11 15:13:05 +0100

    merge social-buttons.js with main.js

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,31 @@ import styles from './../css/main.css';
 //
 // You can also "require" any script from its location in the node modules folder. Webpack often knows what to look for, but you can add a script directly like this:
 // var javascriptthingy = require('name/folder/file.js');
+
+// https://dev.twitter.com/web/javascript/loading
+window.twttr = (function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function (f) {
+      t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+// https://developers.facebook.com/docs/plugins/share-button
+(function (d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s);
+js.id = id;
+js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=366384430479764';
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
