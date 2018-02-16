bc2b2ef8f1081a47a699c9a7b166578e78980857
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-02-16 21:26:09 +0100

    Add fb:app_id

--- static/dist/js/social-buttons.js
+++ static/dist/js/social-buttons.js
@@ -18,9 +18,12 @@ window.twttr = (function (d, s, id) {
 
 // https://developers.facebook.com/docs/plugins/share-button
 (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
+  js = d.createElement(s);
+  js.id = id;
+  js.src =
+    "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=366384430479764&autoLogAppEvents=1";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+})(document, "script", "facebook-jssdk");
