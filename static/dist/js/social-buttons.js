3df6a81ad51db42032a4b22099d7ca32d3f1298b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-02-16 21:05:06 +0100

    Update fb js to v2.12

--- static/dist/js/social-buttons.js
+++ static/dist/js/social-buttons.js
@@ -17,11 +17,10 @@ window.twttr = (function (d, s, id) {
 }(document, "script", "twitter-wjs"));
 
 // https://developers.facebook.com/docs/plugins/share-button
-(function (d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s);
-js.id = id;
-js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=366384430479764';
-fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
