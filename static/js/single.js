fa1d38aa069d018f79fd55cf6b51bb61da2d55ed
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-02-17 19:44:01 +0100

    Fix nav from the beginning and don't scale up

--- static/js/single.js
+++ static/js/single.js
@@ -1,14 +1,5 @@
 "use strict";
 
 var nav = document.querySelector("nav");
-var topOfNav = nav.offsetTop;
-
-function fixNav() {
-  if (window.scrollY > topOfNav) {
-    document.body.classList.add("fixed-nav-parent");
-  } else {
-    document.body.classList.remove("fixed-nav-parent");
-  }
-}
-
-window.addEventListener("scroll", fixNav);
+document.body.classList.add("fixed-nav-parent");
+document.body.style.paddingTop = nav.offsetHeight + "px";
