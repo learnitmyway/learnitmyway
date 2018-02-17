e9c475b50684adbf1eb773187712beb14efe753a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-02-17 09:00:11 +0100

    Fix navbar from the beginning

--- static/dist/js/single.js
+++ static/dist/js/single.js
@@ -5,11 +5,9 @@ var topOfNav = nav.offsetTop;
 
 function fixNav() {
   if (window.scrollY > topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + "px";
     document.body.classList.add("fixed-nav");
   } else {
     document.body.classList.remove("fixed-nav");
-    document.body.style.paddingTop = 0;
   }
 }
 
