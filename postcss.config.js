dbf988ef5fbe89e4099a62736e610eda1c7560c6
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-10-05 23:04:00 +0200

    Only show source maps in dev

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
-module.exports = ctx => ({
-  map: ctx.options.map,
-  plugins: {
-    "postcss-import": {},
-    "postcss-cssnext": {},
-    "cssnano": ctx.env === 'production' ? {autoprefixer: false} : false
+module.exports = ctx => {
+  return {
+    map: ctx.env === 'production' ? false : ctx.options.map, // source maps
+    plugins: {
+      "postcss-import": {},
+      "postcss-cssnext": {},
+      "cssnano": ctx.env === 'production' ? {autoprefixer: false} : false
+    }
   }
-});
+};
